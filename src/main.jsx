5a7dcf43281953b3d944bc98d04b427d49b636b4
Raphael E <raphaelsebastien.evangelista@gmail.com>
2025-10-10 19:54:25 -0500

    implement savings goals context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { EntriesProvider } from './context/EntriesContext.jsx';
+import { SavingsGoalsProvider } from './context/GoalsContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <EntriesProvider>
-      <App />
+      <SavingsGoalsProvider>
+        <App />
+      </SavingsGoalsProvider>
     </EntriesProvider>
   </React.StrictMode>,
 );
